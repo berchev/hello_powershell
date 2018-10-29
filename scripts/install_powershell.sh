782155558f3689bb144d847611bc51ab13632435
Alvaro Miranda Aguilera <kikitux+github@gmail.com>
2018-10-29 14:57:24 +0100

    Update and rename powershell_install.sh to scripts/install_powershell.sh

---
+++ scripts/install_powershell.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+pushd /var/tmp
+
+# add the Microsoft repository GPG keys
+wget -q https://packages.microsoft.com/config/ubuntu/16.04/packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+rm -fr packages-microsoft-prod.deb
+
+# Install PowerShell
+export DEBIAN_FRONTEND=noninteractive
+sudo apt-get update
+sudo apt-get install -y powershell
+sudo apt-get clean
+
+popd
