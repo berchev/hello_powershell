487ab445f7e2ceecdfda0869c644b78e12870330
berchev <43785353+berchev@users.noreply.github.com>
2018-10-30 09:23:35 +0200

    Update install_powershell.sh

--- scripts/install_powershell.sh
+++ scripts/install_powershell.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-# Download the Microsoft repository GPG keys
+# add the Microsoft repository GPG keys
 wget -q https://packages.microsoft.com/config/ubuntu/16.04/packages-microsoft-prod.deb
 
-# Register the Microsoft repository GPG keys
 sudo dpkg -i packages-microsoft-prod.deb
-
-# Update the list of products
-sudo apt-get update
+rm -fr packages-microsoft-prod.deb
 
 # Install PowerShell
+export DEBIAN_FRONTEND=noninteractive
+sudo apt-get update
 sudo apt-get install -y powershell
+sudo apt-get clean
